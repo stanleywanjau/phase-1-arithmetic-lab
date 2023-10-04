f3b94b0da29887f01e9952521e9fbf5887efa2fd
stanleywanjau <stanleywanjau371@gmail.com>
2023-10-04 16:22:17 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2
 
-// Write your code here
+let random=Math.floor(Math.random() * 10) + 1;
+let num3=4
+let num4=20
+let mod=num3%num4
+let max =Math.max(2,2,3,4,20)
